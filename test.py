73b2dbe4e48ce355fd5cf101dac155fe5445cde0
Akshay <ampawar1303@gmail.com>
2023-05-17 10:27:49 +0530

    Intial commit

---
+++ test.py
@@ -0,0 +1,35 @@
+# import json 
+# import csv
+# with open('company_data.json','r') as json_file:
+#     data = json.load(json_file)
+
+# with open('output.tsv', 'w', newline='') as tsv_file:
+#     writer = csv.writer(tsv_file, delimiter='\t')
+
+#     # Write the header
+#     header = data[0].keys()
+#     writer.writerow(header) 
+
+#     # Write the data
+#     for row in data:
+        # writer.writerow(row.values())
+
+
+import json
+import csv
+
+# Open the JSON file for reading
+with open('company_data.json', 'r') as json_file:
+    data = json.load(json_file)['data']
+
+# Open the TSV file for writing
+with open('output.tsv', 'w', newline='') as tsv_file:
+    writer = csv.writer(tsv_file, delimiter='\t')
+
+    # Write the header
+    header = data[0].keys()
+    writer.writerow(header)
+
+    # Write the data
+    for row in data:
+        writer.writerow(row.values())
